4719a250b4c80a25b3276f9e2ed82d74acae305f
Hal Eagar <hal.egar@door3.com>
2018-06-21 12:46:31 -0400

    Start cleaning up Gherkin like requirements from Jira into a feature file

---
+++ src/cypress/support/step_definitions/ck.js
@@ -0,0 +1,16 @@
+/* global cy, then, when, given */
+
+const flow = 'ck/';
+//  /ck/lp
+//  /ck/signup
+//  /ck/form
+//  /ck/portal/home
+
+given('I am a user on the ck flow', () => {
+
+});
+
+
+then('I am displayed the following on the \'No Credit Card Required\' block.', () => {
+
+});
