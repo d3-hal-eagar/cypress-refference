2ebe780bf1f1acd8dface71997cb4bc5f3da16e0
iproskurnia <iaroslav.proskurnia@gmail.com>
2018-07-31 18:38:03 +0300

    Created test for 1 scenario

---
+++ src/cypress/support/step_definitions/ck/ck_back_btn.js
@@ -0,0 +1,15 @@
+// var {defineSupportCode} = require('cucumber');
+//
+// defineSupportCode(function ({And, But, Given, Then, When}) {
+//     When(/^I select the back button located at the top left corner of the browser$/, function (callback) {
+//         callback.pending();
+//     });
+// });
+
+When(`I select the back button located at the top left corner of the browser`, () => {
+    cy.go('back');
+});
+
+Then(`I am guided to the landing screen`, () => {
+    cy.url().should('contain','/ck/landing');
+});
