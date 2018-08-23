dd747855273faeaa122cd832c1f9764be63e5c33
Hal Eagar <hal.egar@door3.com>
2018-08-23 18:20:13 -0400

    Created tests for D3F-46
    also
    removing duplicate tooltip step def's created in a merge

--- src/cypress/support/step_definitions/ck/ck_tooltips.js
+++ src/cypress/support/step_definitions/ck/ck_tooltips.js
@@ -21,22 +21,15 @@ Then(/^"(.*?)" field attribute title is not visible$/, function (formField) {
     cy.getElement(formField).should('have.attr', 'title', '');
 });
 
-Then(/^"(.*?)" tooltip is not visible$/, function (formField) {
-    cy.getFormGroup(formField).find('[role="tooltip"]').should('not.be.visible');
-});
-
-Then(/^I am displayed a attribute title on "(.*?)" field - "(.*?)"$/, function (formField, tooltipText) {
-    cy.getElement(formField).should('have.attr', 'title', tooltipText);
-});
-
-Then(/^"(.*?)" field attribute title is not visible$/, function (formField) {
-    cy.getElement(formField).should('have.attr', 'title', '');
-});
-
 When(/^I hover over "(.*?)"$/, function (formField) {
     cy.getElement(formField).trigger('mouseover');
 });
 
 When(/^I open select "(.*?)"$/, function (formField) {
     //cy.getElement(formField).focus().trigger('mousedown');
+});
+
+When(/^I hover "(.*?)" drop-down/, function (formField) {
+    cy.getElement(formField).focus().select('1989').type('{downarrow}');
+    //cy.getElement(formField).invoke('val', 1989).trigger('mouseover');
 });
